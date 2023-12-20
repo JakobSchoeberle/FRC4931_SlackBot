eb239018c5a14e1c72954ba2608438af30020992
Jakob Schoeberle <105119516+JakobSchoeberle@users.noreply.github.com>
2023-12-20 14:02:35 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import os
 import icalendar
 import recurring_ical_events
 import urllib.request
+import datetime
+import logging
 import schedule
 
-#from slack_bolt import App
-#from dotenv import load_dotenv
+from slack_bolt import App
+from dotenv import load_dotenv
 
 # Initialize the app with the bot token and signing secret
-#load_dotenv()
-#app = App(
-#    token = os.getenv('SLACK_BOT_TOKEN'),
-#    signing_secret = os.getenv('SLACK_SIGNING_SECRET')
-#)
+load_dotenv()
+app = App(
+    token = os.getenv('SLACK_BOT_TOKEN'),
+    signing_secret = os.getenv('SLACK_SIGNING_SECRET')
+)
 
 url = "https://calendar.google.com/calendar/ical/frc4931%40gmail.com/public/basic.ics"
 #Start_date = (2024, 1, 6)
@@ -42,7 +44,28 @@ for event in events:
 
 # Add functionality here
 # @app.event("app_home_opened") etc.
+    
+# Create a timestamp for tomorrow at 9AM
+tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+scheduled_time = datetime.time(hour=9, minute=30)
+schedule_timestamp = datetime.datetime.combine(tomorrow, scheduled_time).strftime('%s')
+
+# Channel you want to post message to
+channel_id = "C12345"
+
+try:
+    # Call the chat.scheduleMessage method using the WebClient
+    result = client.chat_scheduleMessage(
+        channel=channel_id,
+        text="Hello World!",
+        post_at=schedule_timestamp
+    )
+    # Log the result
+    logging.info(result)
+
+except SlackApiError as e:
+    logging.error("Error scheduling message: {}".format(e))
 
 
-#if __name__ == "__main__":
-#    app.start(port=int(os.environ.get("PORT", 3000)))
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
