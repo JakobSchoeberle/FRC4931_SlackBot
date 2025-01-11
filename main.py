442de5f0f1ea2bcdd5cdd51b8a2b46e6f2b7985c
JakobSchoeberle <jrschoeberle@gmail.com>
2025-01-11 15:46:10 -0600

    Updated to Async

--- main.py
+++ main.py
@@ -7,57 +7,42 @@ import logging
 
 SocketMode = True
 
-from slack_bolt import App
+from slack_bolt.async_app import AsyncApp
 
 if SocketMode == True:
-    from slack_bolt.adapter.socket_mode import SocketModeHandler
+    #from slack_bolt.adapter.socket_mode import SocketModeHandler
+    from slack_bolt.adapter.socket_mode.async_handler import AsyncSocketModeHandler
 
 from dotenv import load_dotenv
 load_dotenv()
 
 if SocketMode == True:
-    app = App(token=os.getenv("SLACK_BOT_TOKEN"))
+    app = AsyncApp(token=os.getenv("SLACK_BOT_TOKEN"))
 else:
-    app = App(
+    app = AsyncApp(
         token=os.getenv("SLACK_BOT_TOKEN"),
         signing_secret=os.getenv("SLACK_SIGNING_SECRET")
     )
 
-#url = os.getenv('ical_link')
-##Start_date = (2024, 1, 6)
-##End_date = (2024, 1, 7)
-#a_time = (2024, 1, 6)
-#
-#ical_string = urllib.request.urlopen(url).read()
-#calendar = icalendar.Calendar.from_ical(ical_string)
-##events = recurring_ical_events.of(calendar).between(Start_date, End_date)
-#events = recurring_ical_events.of(calendar).at(a_time)
-
-#for event in events:
-#    name = event["SUMMARY"]
-#    start = event["DTSTART"].dt
-#
-#    try:
-#        description = event["DESCRIPTION"]
-#    except:
-#        description = "N/A" 
-#
-#    try:
-#        location = event["LOCATION"]
-#    except:
-#        location = "N/A" 
-#
-#    print("{} at {} begins at {} - {}".format(name, location, start, description))   
-
 @app.event("app_mention")
-def Responce(event, say):
+async def Responce(event, say):
     welcome_channel_id = os.getenv('channel_id')
     user_id = event["user"]
     text = f"Hey, <@{user_id}>!"
-    say(text=text, channel=welcome_channel_id)
+    await say(text=text, channel=welcome_channel_id)
+
+#if __name__ == "__main__":
+    #if SocketMode == True:
+        #SocketModeHandler(app, os.getenv("SLACK_APP_TOKEN")).start()
+    #else:
+        #app.start(port=int(3000))
+
+async def main():
+    handler = AsyncSocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
+    await handler.start_async()
+
 
 if __name__ == "__main__":
-    if SocketMode == True:
-        SocketModeHandler(app, os.getenv("SLACK_APP_TOKEN")).start()
-    else:
-        app.start(port=int(3000))
+    import asyncio
+
+    asyncio.run(main())
